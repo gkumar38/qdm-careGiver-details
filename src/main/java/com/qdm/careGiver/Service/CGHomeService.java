9274471f00a2644ab6cebdf76a97febcd46d7b8c
gkumar38 <gaurav.lovinglyf@gmail.com>
2020-08-24 13:29:34 +0530

    Changes of Package Name

--- src/main/java/com/qdm/careGiver/Service/CGHomeService.java
+++ src/main/java/com/qdm/careGiver/Service/CGHomeService.java
@@ -1,71 +1,23 @@
 package com.qdm.careGiver.Service;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.qdm.careGiver.Entity.Notifications;
-import com.qdm.careGiver.Entity.Nudges;
-import com.qdm.careGiver.Entity.TotalCount;
-import com.qdm.careGiver.Repository.CGNudgeRepository;
-import com.qdm.careGiver.Repository.NotificationDetailsRepository;
-import com.qdm.careGiver.Repository.TotalCountRepository;
+
 
 
 @Service
-public class CGHomeService {
+public interface CGHomeService {
 	
-	@Autowired
-	CGNudgeRepository cgNudgeRepo;
+	public List<String> getCgNotifications();
 	
-	@Autowired 
-	NotificationDetailsRepository notificationDetails;
+	public Integer totalCount(String type);
 	
-	@Autowired
-	TotalCountRepository totalCountAll;
-	
-	public List<String> getCgNotifications(){
-		Notifications notifications = new Notifications();
-		List<String> notified= null;
-		notificationDetails.getAllNotifications();
-		notifications.setNotifications(notified);
-		return notified;
-	}
-	
-	public Integer totalCount(String type) {
-		Integer totalCountClient =  0;
-		TotalCount total = new TotalCount();
-		totalCountClient = totalCountAll.getTotalClientCount();
-		total.setTotalClient(totalCountClient);
-		return totalCountClient;
-		
-	}
-	
-	public List<String> getNudges(int size){
-		Nudges nudges = new Nudges();
-		List<String> nudgeList= null;
-		nudgeList = cgNudgeRepo.getNudge(size);
-		nudges.setNudges(nudgeList);
-		return nudgeList;
-		
-		
-	}
+	public List<String> getNudges(int size);
 
-	public List<String> getNudgesDetails() {
-		Nudges nudges = new Nudges();
-		List<String> totalNudges= null;
-		totalNudges = cgNudgeRepo.getAllNudges();
-		nudges.setNudges(totalNudges);
-		return totalNudges;
-	}
+	public List<String> getNudgesDetails();
 
-	public Integer totalFeedbackCount(String type) {
-		Integer totalCountFeedback =  0;
-		TotalCount total = new TotalCount();
-		totalCountFeedback = totalCountAll.getTotalFeedBackCount();
-		total.setTotalFeedBack(totalCountFeedback);
-		return totalCountFeedback;
-	}
+	public Integer totalFeedbackCount(String type); 
 	
 
 }
